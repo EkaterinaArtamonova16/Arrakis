10cd870be99fb3f71c1aeace7da91559d0a8c1ef
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-02-26 21:16:24 +0500

    finished 27

---
+++ Seminar 4/task 25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+#region 
+int b = Prompt();
+
+int Prompt()
+{
+    Console.WriteLine("Input number");
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+
+}
+#endregion
+
---
+++ Seminar 4/task 27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int Prompt()
+{
+    Console.WriteLine("Input number");
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+
+}
+int Sum()
+{
+    int b = Prompt();
+    int result = 0;
+    
+    while (b > 0)
+    {
+        result += (b%10);
+        b /= 10;
+
+    }
+    return result;
+}
+Console.WriteLine(Sum());
