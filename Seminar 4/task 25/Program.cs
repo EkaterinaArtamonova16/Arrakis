d7eabc94e9bdd406b82559716cda534d032d85f7
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-02-27 19:34:45 +0500

    finished 25

--- Seminar 4/task 25/Program.cs
+++ Seminar 4/task 25/Program.cs
@@ -4,15 +4,43 @@
 // 2, 4 -> 16
 
 
-#region 
-int b = Prompt();
+// #region 
+//
 
-int Prompt()
+int Base = 1;
+int Exponent = 1;
+int power = 1;
+
+void ReadLine(ref int Base, ref int Exponent)
 {
-    Console.WriteLine("Input number");
-    int a = Convert.ToInt32(Console.ReadLine());
-    return a;
-
+    Console.WriteLine("Input number for exponential ");
+    Base = int.Parse(Console.ReadLine());
+    Console.WriteLine("Input number for exponent ");
+    Exponent = int.Parse(Console.ReadLine());
 }
-#endregion
+int Power(int Base, int Exponent, ref int power)
+{
+    for (int i = 0; i < Exponent; i++)
+    {
+        power *= Base;
+    }
+    return power;
+}
+
+void Validate(int Base, int Exponent, int power)
+{
+    if (Exponent < 0)
+    
+        Console.WriteLine("Input wrong exponent");
+    else
+
+        Console.WriteLine($"Input base number: {Base}, input exponent: {Exponent}. Result is {power}");
+    
+    
+}
+ReadLine(ref Base, ref Exponent);
+Power(Base, Exponent, ref power);
+Validate(Base, Exponent, power);
+
+
 
