c4ca47301618c77c31973f8575ac4a7ae2fda7de
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-02-27 18:13:16 +0500

    finished 29

--- Seminar 4/task 29/Program.cs
+++ Seminar 4/task 29/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] array = new int[8];
+
+int[] GetArray()
+{
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+ 
+}   
+
+void PrintResult()
+{
+    Console.WriteLine($"Result of generation massive: {String.Join("|", array)}");
+}
+
+array = GetArray();
+PrintResult();
