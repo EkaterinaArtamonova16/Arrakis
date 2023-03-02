0e768432c45a77a2948f7b898c94a34f28a8fb82
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-03-02 17:20:34 +0500

    finished 34

--- Seminar 5/Program.cs
+++ Seminar 5/Program.cs
@@ -1,22 +1,132 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Clear();
-Console.WriteLine("1 - task 34");
-Console.WriteLine("2 - task 36");
-Console.WriteLine("3 - task 38");
-
-int NumberOfTask = int.Parse(Console.ReadLine());
-switch (NumberOfTask)
+﻿Console.Clear();
+Console.WriteLine("1 - Task 34");
+Console.WriteLine("2 - Task 36");
+Console.WriteLine("3 - Task 38");
+int NumberOfTask = Prompt("Введите номер задачи");
+switch(NumberOfTask)
 {
-    //case 1:
-       //Task34();
-       //break;
-    //case 2:
-       //Task36();
-       //break;
+    case 1:
+    Console.Clear();
+    Task34();
+    break;
+
+    case 2:
+    Console.Clear();
+    Task36();
+    break;
+
     case 3:
-       Task38();
-       break;
+    Console.Clear();
+    Task38();
+    break;
+
+    default:
+    Console.WriteLine("Номер задачи введен неверно");
+    break;
+    
 }
+    static int Prompt(string massage)//метод запроса числа
+{
+    Console.WriteLine(massage);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+static void Task34()
+{ //Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+    Console.WriteLine("Input lenght array");
+    int lenght = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[lenght];
+    
+    int count = 0;
+
+    int [] RandomNumbersArray()
+    {   
+        int [] arr = new int[lenght];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100,1001);
+    }
+    return arr;
+
+    }
+   void CountEvenNumbers(int[] arr)
+   {
+    foreach (int el in array)
+    {
+        if (el % 2 ==0)
+        {
+    
+            count ++;
+        }
+
+        
+
+    }
+   }
+    
+    void PrintResult(int[] arr, int count)
+    {
+    Console.WriteLine($"Array result is {String.Join(" ", array)}");//Join помогает вывести массив в читаемой форме через разделитель, здесь разделитель 
+    Console.Write($" Number of even numbers {count}");
+    
+    }
+
+
+    array = RandomNumbersArray();
+    CountEvenNumbers(array);
+    PrintResult(array, count);
+}
+
+
+static void Task36()
+{ //Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0З
+
+    Console.WriteLine("Input lenght array");
+    int lenght = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[lenght];
+    int sum = 0;
+
+    int [] RandomNumbersArray()
+    {   
+        int [] arr = new int[lenght];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-100,101);
+    }
+    return arr;
+
+    }
+   void SumNotEvenNum(int[] arr)
+   {
+    foreach (int el in array)
+    {
+        if (el % 2 !=0)
+        {
+            sum ++;
+        }
+    }
+   }
+    
+    void PrintResult(int[] arr, int sum)
+    {
+    Console.WriteLine($"Array result is {String.Join(" ", array)}");//Join помогает вывести массив в читаемой форме через разделитель, здесь разделитель 
+    Console.Write($" Sum of not even numbers {sum}");
+    
+    }
+
+
+    array = RandomNumbersArray();
+    SumNotEvenNum(array);
+    PrintResult(array, sum);
+}
+
 
 static void Task38()
 {
@@ -61,3 +171,5 @@ static void Task38()
     PrintResult(array);
     DifferenceMaxMin(array);
 }
+
+
---
+++ Seminar 6/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+static void Task36()
+{ //Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0З
+
+    Console.WriteLine("Input lenght array");
+    int lenght = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[lenght];
+    int sum = 0;
+
+    int [] RandomNumbersArray()
+    {   
+        int [] arr = new int[lenght];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0,101);
+    }
+    return arr;
+
+    }
+   void SumNotEvenNum(int[] arr)
+   {
+    foreach (int el in array)
+    {
+        if (el % 2 !=0)
+        {
+            sum ++;
+        }
+        else
+        {
+            Console.WriteLine("We don't need even numbers");
+        }
+
+        
+
+    }
+   }
+    
+    void PrintResult(int[] arr, int sum)
+    {
+    Console.WriteLine($"Array result is {String.Join(" ", array)}");//Join помогает вывести массив в читаемой форме через разделитель, здесь разделитель 
+    Console.Write($" Sum of not even numbers {sum}");
+    
+    }
+
+
+    array = RandomNumbersArray();
+    SumNotEvenNum(array);
+    PrintResult(array, sum);
+}
