4852401d20ec94a11399951e1f07f49f3ee1ffef
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-03-03 14:16:46 +0500

    new task added for h/w

--- Seminar 6/Program.cs
+++ Seminar 6/Program.cs
@@ -1,52 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-static void Task36()
-{ //Задайте одномерный массив, заполненный случайными числами. 
-//Найдите сумму элементов, стоящих на нечётных позициях.
-//[3, 7, 23, 12] -> 19
-//[-4, -6, 89, 6] -> 0З
+int[,] array = new int[new Random().Next(2,10), new Random().Next(2,10)];
 
-    Console.WriteLine("Input lenght array");
-    int lenght = Convert.ToInt32(Console.ReadLine());
-    int[] array = new int[lenght];
-    int sum = 0;
-
-    int [] RandomNumbersArray()
-    {   
-        int [] arr = new int[lenght];
-    for (int i = 0; i < arr.Length; i++)
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        arr[i] = new Random().Next(0,101);
-    }
-    return arr;
-
-    }
-   void SumNotEvenNum(int[] arr)
-   {
-    foreach (int el in array)
-    {
-        if (el % 2 !=0)
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            sum ++;
+            array[i,j] = new Random().Next(-10,11);
+            Console.WriteLine(array[i,j] + " ");
         }
-        else
-        {
-            Console.WriteLine("We don't need even numbers");
-        }
-
-        
-
     }
-   }
-    
-    void PrintResult(int[] arr, int sum)
-    {
-    Console.WriteLine($"Array result is {String.Join(" ", array)}");//Join помогает вывести массив в читаемой форме через разделитель, здесь разделитель 
-    Console.Write($" Sum of not even numbers {sum}");
-    
-    }
-
-
-    array = RandomNumbersArray();
-    SumNotEvenNum(array);
-    PrintResult(array, sum);
 }
+
+void Explorer(int[,] array)
+{
+    for (int i = 0; i <array.GetLength; i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j <array.GetLength; j++)
+        {
+            if(i%2 == 0 && j%2 == 0)
+            array[i,j] *= array[i,j];
+
+        }
+        Console.Write(array[i,j] + " ");
+
+    }
+}
+FillArray(array);
+Console.WriteLine();
+Explorer(array);
---
+++ seminar 7/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input number");
+int ViewerNumber = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[new Random().Next(5,6), new Random().Next(5,6)];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {  
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 11);
+            Console.Write(array[i, j] + " ");
+        }
+    }   
+}
+
+void Viewer(int[,] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(array[i,j] == ViewerNumber)
+            {
+                Console.WriteLine($"Element is taking is on: {i}, {j}");
+            }
+            else
+            {
+                count++;
+            }
+        }
+    }
+    
+    if (count == array.GetLength(0)*array.GetLength(1));
+    Console.WriteLine("There is no such element");
+}
+
+FillArray(array);
+Console.WriteLine();
+Viewer(array);
