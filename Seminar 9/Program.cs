a857db231e4afaeeac8189c5cb919a777f6dbca9
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-03-24 13:36:26 +0500

    finished touch ups

--- Seminar 9/Program.cs
+++ Seminar 9/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.Clear();
 Console.WriteLine("1 - Task 66");
-Console.WriteLine("2 - Task 68");
+Console.WriteLine("2 - Task 64");
+Console.WriteLine("3 - Task 68");
 int NumberOfTask = Prompt("Введите номер задачи");
 switch(NumberOfTask)
 {
@@ -9,15 +10,15 @@ switch(NumberOfTask)
     Task66();
     break;
 
-    //case 2:
-    //Console.Clear();
-    //Task68();
-    //break;
+    case 2:
+    Console.Clear();
+    Task64();
+    break;
 
-    //case 3:
-    //Console.Clear();
-    //Task38();
-    //break;
+    case 3:
+    Console.Clear();
+    Task68();
+    break;
 
     default:
     Console.WriteLine("Номер задачи введен неверно");
@@ -52,20 +53,44 @@ int FinfSumAllElements(int firstnum, int secondnum)
 
 }
 
-//static void Task68()
-{ //Задача 68: Напишите программу вычисления функции Аккермана 
-//с помощью рекурсии. Даны два неотрицательных числа m и n. 
-//m = 2, n = 3 -> A(m,n) = 29
+static void Task64()
+{ 
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
-static uint A(uint n, uint m)
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+System.Console.WriteLine("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int PrintNumbers(int n)
 {
-  if (n == 0)
-    return m + 1;
-  else
-    if ((n != 0) && (m == 0))
-      return A(n - 1, 1);
-    else
-      return A(n - 1, A(n, m - 1));
+    if (n == 1) return 1;
+    Console.Write($"{n}, ");
+    return PrintNumbers(n-1);
 }
 
+System.Console.WriteLine(PrintNumbers(n));
+
 } 
+
+static void Task68()
+{ 
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+// m = 2, n = 3 -> A(m,n) = 29
+
+System.Console.WriteLine("Input minimal number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ackermann(m - 1, 1);
+    else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+System.Console.WriteLine(Ackermann(m,n));
+
+
+}
--- Test /Program.cs
+++ Test /Program.cs
@@ -1,11 +1,1 @@
-﻿System.Console.WriteLine("Input number: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-int PrintNumbers(int n)
-{
-    if (n == 1) return 1;
-    Console.Write($"{n}, ");
-    return PrintNumbers(n-1);
-}
-
-System.Console.WriteLine(PrintNumbers(n));
+﻿
